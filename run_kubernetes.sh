df1e5e7ae9f0fe85d4742f6c428648caed0ba694
Osayi Akoko <osayistreams@gmail.com>
2022-07-29 11:00:35 +0100

    update run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="osayistreams/house-price-prediction-ml-api:v1.0.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy house-price-prediction-ml-api --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/house-price-prediction-ml-api-5bbdbffdc4-v9bsr --address 0.0.0.0 8000:80
