5e838f2676770e4b38fe4fcef7840cdd0e933a65
Osayi Akoko <osayistreams@gmail.com>
2022-07-29 10:59:28 +0100

    update upload_docker script

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="osayistreams/house-price-prediction-ml-api:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag house-price-prediction-ml-api $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
